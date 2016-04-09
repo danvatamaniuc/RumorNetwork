cb4fccc62a68bff388ecda2018070cc5023278f6
danvatamaniuc <da.vatamaniuc@gmail.com>
2016-04-09 16:30:30 +0300

    Initial commit of source files
    Uploading base source files to git.

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace RumorNetwork
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMembersNumber = new System.Windows.Forms.Label();
+            this.txtMembersNumber = new System.Windows.Forms.TextBox();
+            this.cmdGenerateMembers = new System.Windows.Forms.Button();
+            this.rbnEA = new System.Windows.Forms.RadioButton();
+            this.rbnPSO = new System.Windows.Forms.RadioButton();
+            this.cmdSolve = new System.Windows.Forms.Button();
+            this.cmdClear = new System.Windows.Forms.Button();
+            this.dgvMembersRelation = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMembersRelation)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblMembersNumber
+            // 
+            this.lblMembersNumber.AutoSize = true;
+            this.lblMembersNumber.Location = new System.Drawing.Point(12, 9);
+            this.lblMembersNumber.Name = "lblMembersNumber";
+            this.lblMembersNumber.Size = new System.Drawing.Size(50, 13);
+            this.lblMembersNumber.TabIndex = 0;
+            this.lblMembersNumber.Text = "Members";
+            // 
+            // txtMembersNumber
+            // 
+            this.txtMembersNumber.Location = new System.Drawing.Point(68, 6);
+            this.txtMembersNumber.Name = "txtMembersNumber";
+            this.txtMembersNumber.Size = new System.Drawing.Size(27, 20);
+            this.txtMembersNumber.TabIndex = 1;
+            // 
+            // cmdGenerateMembers
+            // 
+            this.cmdGenerateMembers.Location = new System.Drawing.Point(101, 4);
+            this.cmdGenerateMembers.Name = "cmdGenerateMembers";
+            this.cmdGenerateMembers.Size = new System.Drawing.Size(75, 23);
+            this.cmdGenerateMembers.TabIndex = 2;
+            this.cmdGenerateMembers.Text = "Generate";
+            this.cmdGenerateMembers.UseVisualStyleBackColor = true;
+            this.cmdGenerateMembers.Click += new System.EventHandler(this.cmdGenerateMembers_Click);
+            // 
+            // rbnEA
+            // 
+            this.rbnEA.AutoSize = true;
+            this.rbnEA.Location = new System.Drawing.Point(263, 7);
+            this.rbnEA.Name = "rbnEA";
+            this.rbnEA.Size = new System.Drawing.Size(39, 17);
+            this.rbnEA.TabIndex = 8;
+            this.rbnEA.TabStop = true;
+            this.rbnEA.Text = "EA";
+            this.rbnEA.UseVisualStyleBackColor = true;
+            // 
+            // rbnPSO
+            // 
+            this.rbnPSO.AutoSize = true;
+            this.rbnPSO.Location = new System.Drawing.Point(308, 7);
+            this.rbnPSO.Name = "rbnPSO";
+            this.rbnPSO.Size = new System.Drawing.Size(47, 17);
+            this.rbnPSO.TabIndex = 9;
+            this.rbnPSO.TabStop = true;
+            this.rbnPSO.Text = "PSO";
+            this.rbnPSO.UseVisualStyleBackColor = true;
+            // 
+            // cmdSolve
+            // 
+            this.cmdSolve.Location = new System.Drawing.Point(361, 4);
+            this.cmdSolve.Name = "cmdSolve";
+            this.cmdSolve.Size = new System.Drawing.Size(75, 23);
+            this.cmdSolve.TabIndex = 10;
+            this.cmdSolve.Text = "Solve";
+            this.cmdSolve.UseVisualStyleBackColor = true;
+            this.cmdSolve.Click += new System.EventHandler(this.cmdSolve_Click);
+            // 
+            // cmdClear
+            // 
+            this.cmdClear.Location = new System.Drawing.Point(182, 4);
+            this.cmdClear.Name = "cmdClear";
+            this.cmdClear.Size = new System.Drawing.Size(75, 23);
+            this.cmdClear.TabIndex = 11;
+            this.cmdClear.Text = "Clear";
+            this.cmdClear.UseVisualStyleBackColor = true;
+            this.cmdClear.Click += new System.EventHandler(this.cmdClear_Click);
+            // 
+            // dgvMembersRelation
+            // 
+            this.dgvMembersRelation.AllowUserToAddRows = false;
+            this.dgvMembersRelation.AllowUserToDeleteRows = false;
+            this.dgvMembersRelation.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMembersRelation.Location = new System.Drawing.Point(361, 33);
+            this.dgvMembersRelation.Name = "dgvMembersRelation";
+            this.dgvMembersRelation.Size = new System.Drawing.Size(375, 339);
+            this.dgvMembersRelation.TabIndex = 12;
+            this.dgvMembersRelation.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMembersRelation_CellValueChanged);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(748, 384);
+            this.Controls.Add(this.dgvMembersRelation);
+            this.Controls.Add(this.cmdClear);
+            this.Controls.Add(this.cmdSolve);
+            this.Controls.Add(this.rbnPSO);
+            this.Controls.Add(this.rbnEA);
+            this.Controls.Add(this.cmdGenerateMembers);
+            this.Controls.Add(this.txtMembersNumber);
+            this.Controls.Add(this.lblMembersNumber);
+            this.Name = "MainWindow";
+            this.Text = "Rumor Network";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMembersRelation)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMembersNumber;
+        private System.Windows.Forms.TextBox txtMembersNumber;
+        private System.Windows.Forms.Button cmdGenerateMembers;
+        private System.Windows.Forms.RadioButton rbnEA;
+        private System.Windows.Forms.RadioButton rbnPSO;
+        private System.Windows.Forms.Button cmdSolve;
+        private System.Windows.Forms.Button cmdClear;
+        private System.Windows.Forms.DataGridView dgvMembersRelation;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RumorNetwork.Logic;
+using RumorNetwork.Validator;
+
+namespace RumorNetwork
+{
+    public partial class MainWindow : Form
+    {
+
+        private Dictionary<int, float[]> members;
+        private int _memberNr;
+
+        public MainWindow()
+        {
+            members = new Dictionary<int, float[]>();
+            _memberNr = 0;
+            InitializeComponent();
+        }
+
+        private void cmdGenerateMembers_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _memberNr = int.Parse(txtMembersNumber.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Number of members could not been extracted!");
+                return;
+            }
+            
+            DrawMembers();
+        }
+
+        private void DrawMembers()
+        {
+            if (_memberNr.Equals(0))
+            {
+                return;
+            }
+
+            Pen memberPen = new Pen(Color.Black);
+            Graphics formGraphics = CreateGraphics();
+
+            members.Clear();
+
+            GenerateRelationalGrid(_memberNr);
+
+            //x and y coords of the circle of members
+            int xCoord = 150;
+            int yCoord = 190;
+
+            int anglePace = 360/_memberNr;
+            int radius = 100;
+
+            int angle = 0;
+
+
+            //draw all the members in a circle
+            for (int i = 0; i < _memberNr; i++)
+            {
+                float pointX = xCoord + (float) (radius*Math.Sin(Math.PI*angle/180.0));
+                float pointY = yCoord + (float) (radius*Math.Cos(Math.PI*angle/180.0));
+
+                angle += anglePace;
+
+                formGraphics.DrawEllipse(memberPen, pointX, pointY, 4, 4);
+                members.Add(i, new[] {pointX, pointY});
+            }
+
+            memberPen.Dispose();
+            formGraphics.Dispose();
+        }
+
+        /// <summary>
+        /// Creates and populates the grid used to describe relations
+        /// </summary>
+        /// <param name="memberNr">Number of members</param>
+        private void GenerateRelationalGrid(int memberNr)
+        {
+            dgvMembersRelation.Columns.Clear();
+
+            for (int i = 0; i < memberNr; i++)
+            {
+                dgvMembersRelation.Columns.Add((i + 1).ToString(), (i + 1).ToString());
+                dgvMembersRelation.Columns[i].Width = 20;
+                dgvMembersRelation.Rows.Add();
+            }
+
+            foreach (DataGridViewRow row in dgvMembersRelation.Rows)
+            {
+                for (int i = 0; i < memberNr; i++)
+                {
+                    row.Cells[i].Value = 0;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Clears the drawing and the relational grid
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cmdClear_Click(object sender, EventArgs e)
+        {
+            _memberNr = 0;
+            Invalidate();
+            dgvMembersRelation.Columns.Clear();
+        }
+
+        private void dgvMembersRelation_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            string newValue = dgvMembersRelation.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString();
+
+            //if the new value is not 0 and its compliment is 0, new connection added
+            //otherwise just delete the connection
+            if (
+                !newValue.Equals("0")
+                &&
+                dgvMembersRelation.Rows[e.ColumnIndex].Cells[e.RowIndex].Value.ToString().Equals("0"))
+            {
+                DrawConnection(e.ColumnIndex, e.RowIndex, Color.Black);
+                dgvMembersRelation.Rows[e.ColumnIndex].Cells[e.RowIndex].Value = newValue;
+            }
+            else if (newValue.Equals("0"))
+            {
+                dgvMembersRelation.Rows[e.ColumnIndex].Cells[e.RowIndex].Value = newValue;
+            }
+        }
+
+        private void DrawConnection(int columnIndex, int rowIndex, Color connectionColor)
+        {
+            //initialize the drawing classes
+            Pen connectionPen = new Pen(connectionColor);
+            Graphics formGraphics = CreateGraphics();
+
+            //get the points coords
+            float[] memberACoords, memberBCoords;
+
+            if (!members.TryGetValue(columnIndex, out memberACoords))
+            {
+                MessageBox.Show("Error while getting coords for point A!");
+                return;
+            }
+
+            if (!members.TryGetValue(rowIndex, out memberBCoords))
+            {
+                MessageBox.Show("Error while getting coords for point B!");
+                return;
+            }
+
+            //draw the line
+            formGraphics.DrawLine(connectionPen, memberACoords[0], memberACoords[1], memberBCoords[0], memberBCoords[1]);
+
+            //dispose of the drawing classes
+            connectionPen.Dispose();
+            formGraphics.Dispose();
+        }
+
+        private void cmdSolve_Click(object sender, EventArgs e)
+        {
+            List<int[]> relations = new List<int[]>();
+
+            for (int i = 0; i < _memberNr; i++)
+            {
+                for (int j = i; j < _memberNr; j++)
+                {
+                    if (j != i && !dgvMembersRelation.Rows[i].Cells[j].Value.ToString().Equals("0"))
+                    {
+                        relations.Add(new int[] {i, j});
+                    }
+                }
+            }
+
+            if (relations.Count.Equals(0))
+            {
+                MessageBox.Show("No members and relations defined!");
+                return;
+            }
+
+            List<int[]> newRelations = null;
+              
+            try
+            {
+                if (rbnEA.Checked)
+                {
+                    newRelations = Solver.EvolutiveSearch(relations, _memberNr);
+                }
+                else
+                {
+                    newRelations = Solver.ParticleSwarmOptimization(relations, _memberNr);
+                }
+                
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                return;
+            }
+
+            if (newRelations == null)
+            {
+                MessageBox.Show("A solution has not been found!");
+                return;
+            }
+
+            DrawMembers();
+
+            foreach (int[] relation in newRelations)
+            {
+                dgvMembersRelation.Rows[relation[0]].Cells[relation[1]].Value = "1";
+                DrawConnection(relation[0], relation[1], Color.Aqua);
+            }
+        }
+    }
+}
